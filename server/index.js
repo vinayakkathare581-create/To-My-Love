5ef9efd44f295785350dcb6ff5ffc0bc7aadb793
vinayak592 <vinnikathare123@gmail.com>
2026-05-03 00:12:57 +0530

    Fix Express wildcard route syntax for production

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use('/api', aiRouter);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
 }
